cac50d134717d7fb27940cae91dccc59d80b1ebf
NotMarvel <80026033+JustMarvell@users.noreply.github.com>
2025-02-23 19:37:00 +0800

    setup characters.py and settings.py

---
+++ controllers/characters.py
@@ -0,0 +1,21 @@
+import connections.dbConnect as db
+from discord.ext import commands
+
+@commands.hybrid_command()
+async def get_characters_list(ctx):
+    mycursor = await db.Connect()
+    
+    sql = "SELECT char_name FROM characters"
+    
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    
+    charlist = []
+    for char in myresult:
+        charlist += char
+    mycursor.close()
+    
+    await ctx.response.send_message(charlist)
+    
+async def setup(bot):
+    bot.add_command(get_characters_list)
--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
 BASE_DIR = pathlib.Path(__file__).parent
+CONTROLER_COMMAND_DIR = BASE_DIR / "controllers"
 
 LOGGING_CONFIG = {
     "version" : 1,
